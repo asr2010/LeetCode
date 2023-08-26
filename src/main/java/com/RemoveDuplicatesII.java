86f1ad13cdd4d828d3e53bebd8f1a313f42de449
asr2010 <arpitshekharraj@gmail.com>
2023-08-26 18:23:40 -0400

    Added ListNode class to remove errors from local IDE

--- src/main/java/com/RemoveDuplicatesII.java
+++ src/main/java/com/RemoveDuplicatesII.java
@@ -28,6 +28,14 @@ package com;
  * }
  */
 
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
 public class RemoveDuplicatesII{
     public ListNode deleteDuplicates(ListNode head) {
 
